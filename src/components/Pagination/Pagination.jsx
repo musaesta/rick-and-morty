ae35ec1ebf41688873d143541e05b7a200e82e91
Mona Lisa <musa.estamirov@mail.ru>
2024-07-26 18:26:08 +0300

    last

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,6 +1,26 @@
-import * as React from "react";
-import Pagination from "@mui/material/Pagination";
+// Img
+import leftArrow from "./img/leftArrow.svg";
+import rightArrow from "./img/rightArrow.svg";
+// Styles
+import styles from "./Pagination.module.css";
 
-export const CustomIcons = ({ pages }) => {
-  return <Pagination count={pages} size="large" variant="outlined" />;
+export const Pagination = ({ handleClickPage, nextPage, prevPage }) => {
+  return (
+    <div className={styles.container}>
+      <button
+        onClick={() => handleClickPage(prevPage)}
+        className={styles.btn}
+        disabled={!prevPage}
+      >
+        <img className={styles.icon} src={leftArrow} alt="left-arrow" />
+      </button>
+      <button
+        onClick={() => handleClickPage(nextPage)}
+        className={styles.btn}
+        disabled={!nextPage}
+      >
+        <img className={styles.icon} src={rightArrow} alt="right-arrow" />
+      </button>
+    </div>
+  );
 };
--- src/containers/CharactersPage/CharactersPage.jsx
+++ src/containers/CharactersPage/CharactersPage.jsx
@@ -4,25 +4,39 @@ import { getData } from "../../services/service";
 import { baseUrl, characterUrl } from "../../constants/api";
 // components
 import { CharactersList } from "../../components/CharactersPage/CharactersList";
-import { CustomIcons } from "../../components/Pagination/Pagination";
+import { Pagination } from "../../components/Pagination/Pagination";
 
 export const CharactersPage = () => {
   const [characters, setCharacters] = useState([]);
-  const [pages, setPages] = useState();
+  const [page, setPage] = useState("");
+  const [prevPage, setPrevPage] = useState();
+  const [nextPage, setNextPage] = useState();
   const getCharacters = async (url) => {
     const { results, info } = await getData(url);
-    const { pages } = info;
-    setPages(pages);
+    const { next, prev } = info;
+    setNextPage(next);
+    setPrevPage(prev);
     setCharacters(results);
   };
 
+  const handleClickPage = (changePage) => {
+    if (changePage) {
+      const index = changePage.indexOf("?page=");
+      setPage(changePage.slice(index));
+    }
+  };
+
   useEffect(() => {
-    getCharacters(baseUrl + characterUrl);
-  }, []);
+    getCharacters(baseUrl + characterUrl + page);
+  }, [page]);
 
   return (
     <div>
-      <CustomIcons pages={pages} />
+      <Pagination
+        prevPage={prevPage}
+        nextPage={nextPage}
+        handleClickPage={handleClickPage}
+      />
       <CharactersList characters={characters} />
     </div>
   );
