36824a4560917fd7598ccd8b25d4ce31c678c160
Mona Lisa <musa.estamirov@mail.ru>
2024-07-27 02:59:59 +0300

    HOC

--- src/containers/CharactersPage/CharactersPage.jsx
+++ src/containers/CharactersPage/CharactersPage.jsx
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // api
-import { getData } from '../../utils/network';
-import { getPageId } from '../../services/getPageId';
-import { baseUrl, characterUrl } from '../../constants/api';
+import { getData } from "../../utils/network";
+import { getPageId } from "../../services/getPageId";
+import { baseUrl, characterUrl } from "../../constants/api";
 // components
-import { CharactersList } from '../../components/CharactersPage/CharactersList';
-import { Pagination } from '../../components/Pagination/Pagination';
+import { CharactersList } from "../../components/CharactersPage/CharactersList";
+import { Pagination } from "../../components/Pagination/Pagination";
 
 export const CharactersPage = () => {
-	const [characters, setCharacters] = useState([]);
-	const [page, setPage] = useState('');
-	const [prevPage, setPrevPage] = useState(null);
-	const [nextPage, setNextPage] = useState(null);
-	const getCharacters = async url => {
-		const data = await getData(url);
-		if (data) {
-			const { results, info } = data;
-			const { next, prev } = info;
-			setNextPage(next);
-			setPrevPage(prev);
-			setCharacters(results);
-		}
-	};
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState("");
+  const [prevPage, setPrevPage] = useState(null);
+  const [nextPage, setNextPage] = useState(null);
+  const getCharacters = async (url) => {
+    const data = await getData(url);
+    if (data) {
+      const { results, info } = data;
+      const { next, prev } = info;
+      setNextPage(next);
+      setPrevPage(prev);
+      setCharacters(results);
+    }
+  };
 
-	const handleClickPage = changePage => setPage(getPageId(changePage));
+  const handleClickPage = (changePage) => setPage(getPageId(changePage));
 
-	useEffect(() => {
-		getCharacters(baseUrl + characterUrl + page);
-	}, [page]);
+  useEffect(() => {
+    getCharacters(baseUrl + characterUrl + page);
+  }, [page]);
 
-	return (
-		<div>
-			{characters.length > 0 ? (
-				<Pagination
-					prevPage={prevPage}
-					nextPage={nextPage}
-					handleClickPage={handleClickPage}
-				/>
-			) : null}
-			<CharactersList characters={characters} />
-		</div>
-	);
+  return (
+    <div>
+      {characters.length > 0 ? (
+        <Pagination
+          prevPage={prevPage}
+          nextPage={nextPage}
+          handleClickPage={handleClickPage}
+        />
+      ) : null}
+      <CharactersList characters={characters} />
+    </div>
+  );
 };
